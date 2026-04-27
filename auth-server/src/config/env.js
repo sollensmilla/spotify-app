389e60576e1743fba52fb55595b2bbbb0eb2033d
Smilla Sollén <ss226uk@student.lnu.se>
2026-04-27 21:09:55 +0200

    Change naming

--- auth-server/src/config/env.js
+++ auth-server/src/config/env.js
@@ -8,8 +8,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const {
-  CLIENT_ID,
-  CLIENT_SECRET,
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
   API_URL
