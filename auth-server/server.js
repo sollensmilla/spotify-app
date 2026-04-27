a97982b69eca4a3698a7dbff29f67f257d9454cf
Smilla Sollén <ss226uk@student.lnu.se>
2026-04-27 13:03:57 +0200

    More robust cors

--- auth-server/server.js
+++ auth-server/server.js
@@ -9,12 +9,25 @@ app.use(cookieParser())
 
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://spotify-app-production-6203.up.railway.app'
+]
+
 app.use(cors({
-  origin: FRONTEND_URL,
+  origin: function (origin, callback) {
+    console.log('Incoming origin:', origin)
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   credentials: true
 }))
 
-// app.use('/auth', authRoutes)
+app.use('/auth', authRoutes)
 
 app.get('/', (req, res) => {
   res.send('Auth server is running')
