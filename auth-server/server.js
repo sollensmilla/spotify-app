78a3a09915ef30699fb985fcba8c9c9f1ea0408a
Smilla Sollén <ss226uk@student.lnu.se>
2026-04-27 13:24:56 +0200

    debug

--- auth-server/server.js
+++ auth-server/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import authRoutes from './src/routes/authRoutes.js'
+// import authRoutes from './src/routes/authRoutes.js'
 
 const app = express()
 
